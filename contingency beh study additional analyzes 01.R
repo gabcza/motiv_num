e60368c15a0763709de4bea17c05f4530e6f9b1d
Iwonka <iwona.dudek93@gmail.com>
2024-04-11 16:12:25 +0200

    Series of linear regression analyses with science beliefs as DV

---
+++ contingency beh study additional analyzes 01.R
@@ -0,0 +1,72 @@
+#---- Info -----
+# Goal: corrborate result of the contingency beh study with
+# science beliefs study (i.e. a series of linear regression analyses with s
+# science beliefs (climate change, GMO, homeopathy as a DV and cultural ideology as a predictor 
+# + interation with sophistication)
+#
+# write 11-04-2024 by Iwona
+#--------------------------------------------------------------------------------
+#---- Load packages ----
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+library(ggeffects)
+options(scipen = 999)
+
+#---- Load data in a long format ----
+# use the script called 'contingency beh study 01 clean data.R"
+# or saved data
+#data.long <- read.csv(data contingency beh study pilot clean long data.csv")
+
+
+#----Main effect of ideology 
+#Climat
+r1.climate <- lm(data = data.long, prior.climate.pos ~ age_s_c + gender + educ_s_c + ideology)    
+summary(r1.climate)
+r1.climate.means <- ggemmeans(r1.climat, terms = "ideology")
+plot(r1.climate.means)
+car::vif(r1.climate)
+
+#GMO
+r1.gmo <- lm(data = data.long, prior.gmo.pos ~ age_s_c + gender + educ_s_c + ideology)    
+summary(r1.gmo)
+r1.gmo.means <- ggemmeans(r1.gmo, terms = "ideology")
+plot(r1.gmo.means)
+car::vif(r1.gmo)
+
+#homeopathy 
+r1.hom <- lm(data = data.long, prior.hom.pos ~ age_s_c + gender + educ_s_c + ideology)    
+summary(r1.hom)
+r1.hom.means <- ggemmeans(r1.hom, terms = "ideology")
+plot(r1.hom.means)
+car::vif(r1.hom)
+
+
+#Interaction: ideology x num
+
+#Climat
+r2.climate <- lm(data = data.long, prior.climate.pos ~ age_s_c + gender + educ_s_c + ideology * num_c)    
+summary(r2.climate)
+r2.climate %>% ggemmeans(terms = c("num_c", "ideology")) %>% plot()
+car::vif(r2.climate)
+
+#GMO
+r2.gmo <- lm(data = data.long, prior.gmo.pos ~ age_s_c + gender + educ_s_c + ideology * num_c)    
+summary(r2.gmo)
+r2.gmo %>% ggemmeans(terms = c("num_c", "ideology")) %>% plot()
+car::vif(r2.gmo)
+
+#homeopathy
+r2.hom <- lm(data = data.long, prior.hom.pos ~ age_s_c + gender + educ_s_c + ideology * num_c)    
+summary(r2.hom)
+r2.hom %>% ggemmeans(terms = c("num_c", "ideology")) %>% plot()
+car::vif(r2.hom)
+
+#summary
+#for polit cult 
+sjPlot::tab_model(r1.climate, r1.gmo, r1.hom) 
+#for polit cult x num
+sjPlot::tab_model(r2.climate, r2.gmo, r2.hom)
